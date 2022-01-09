257dd62fc58e050db13b64a4a821a02721c75d3f
rasto <rasto.skubal@gmail.com>
2022-01-09 09:37:33 +0100

    test class

--- mdblib.js
+++ mdblib.js
@@ -10,3 +10,24 @@ const pad = (number, length) => {
 const remoteMessage = ((mes) => {
     message("Remote script: " + mes);
 });
+
+class Dochadzka {
+    constructor(entry) {
+        this.lib = "Dochádzka";
+        this.datum = entry.field("Dátum");
+        this.prichod = entry.field("Príchod");
+        this.odchod = entry.field("Odchod");
+        this.zamestnanci = entry.field("Zamestnanci");
+        this.prace = entry.field("Práce");
+        // vypočítané
+        this.odpracovane = entry.field("Odpracované");
+        this.pracovnaDoba = entry.field("Pracovná doba");
+        this.naZakazkach = entry.field("Na zákazkach");
+        this.prestoje = entry.field("Prestoje");
+        this.mzdoveNaklady = entry.field("Mzdové náklady");
+    }
+    library() {
+        message("Knižnica je:" + this.lib);
+        return this;
+    }
+}
