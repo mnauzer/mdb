4e2d8e74767abdaa84e881b171b12896dbfadcef
rasto <rasto.skubal@gmail.com>
2022-01-09 09:51:15 +0100

    updates

--- mdblib.js
+++ mdblib.js
@@ -1,33 +1,24 @@
 // padding number
-const pad = (number, length) => {
+const pad = ((number, length) => {
     let str = '' + number;
     while (str.length < length) {
         str = '0' + str;
     }
     return str;
-};
-
-const remoteMessage = ((mes) => {
-    message("Remote script: " + mes);
 });
 
-class Dochadzka {
-    constructor(entry) {
-        this.lib = "Dochádzka";
-        this.datum = entry.field("Dátum");
-        this.prichod = entry.field("Príchod");
-        this.odchod = entry.field("Odchod");
-        this.zamestnanci = entry.field("Zamestnanci");
-        this.prace = entry.field("Práce");
-        // vypočítané
-        this.odpracovane = entry.field("Odpracované");
-        this.pracovnaDoba = entry.field("Pracovná doba");
-        this.naZakazkach = entry.field("Na zákazkach");
-        this.prestoje = entry.field("Prestoje");
-        this.mzdoveNaklady = entry.field("Mzdové náklady");
-    }
-    library() {
-        message("Knižnica je:" + this.lib);
-        return this;
-    }
-}
+const roundTimeQ = (time => {
+    var timeToReturn = new Date(time);
+
+    timeToReturn.setMilliseconds(Math.round(timeToReturn.getMilliseconds() / 1000) * 1000);
+    timeToReturn.setSeconds(Math.round(timeToReturn.getSeconds() / 60) * 60);
+    timeToReturn.setMinutes(Math.round(timeToReturn.getMinutes() / 15) * 15);
+    return timeToReturn;
+
+});
+
+const remoteMessage = (
+    (mes) => {
+        message("Remote script: " + mes);
+    });
+
