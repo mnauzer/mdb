2e70578ca3219c7d26a1082e71e4eea07a562e90
mnauzer <76249162+mnauzer@users.noreply.github.com>
2022-01-10 18:08:54 +0100

    calcZam update

--- mdblib.js
+++ mdblib.js
@@ -24,8 +24,8 @@ const remoteMessage = (
     });
 
 // Popis:                   prepočíta dáta zamestnanca
-const calcZam = ((zamestnanec, library) => {
-    message("Verzia: " + version + " Output: " + library);
+const calcZam = ((zamestnanec, libraries) => {
+    message("Verzia: " + version + " Output: " + libraries);
 
     var nick = zamestnanec.field("Nick");
     var hs = zamestnanec.field("Hodinovka"); // nastavená hodinová sadzba (hs)
@@ -39,8 +39,9 @@ const calcZam = ((zamestnanec, library) => {
         nastaveneSezony.push(sz[s]);
     }
     nastaveneSezony.forEach((sezona) => {
+
         // Dochádzka
-        if (library.includes("Dochádzka")) {
+        if ((libraries.find(library => library == "Dochádzka")) >= 0) {
             var odpracovaneCelkom = 0; // odpracované hodiny celkom => Dochádzka
             var zarobeneCelkom = 0; // zarobená suma, odpracované hodiny x hodinová mzda => Dochádzka
             var hodnotenieCelkom = 0; // počet hviezdičiek => Dochádzka
@@ -67,7 +68,7 @@ const calcZam = ((zamestnanec, library) => {
             zamestnanec.set("Preplatok/Nedoplatok", zarobeneCelkom - zamestnanec.field("Vyplatené"));
         }
         // Mzdy
-        if (library.includes("Platby")) {
+        if ((libraries.find(library => library == "Platby")) >= 0) {
             var vyplateneCelkom = 0; // vyplatené mzdy => Pokladňa
             lfPlatby = zamestnanec.linksFrom("Pokladňa", "Zamestnanec");
             if (lfPlatby) {
@@ -83,7 +84,7 @@ const calcZam = ((zamestnanec, library) => {
         }
 
         // Evidencia prác
-        if (library.includes("Práce")) {
+        if ((libraries.find(library => library == "Práce")) >= 0) {
             var evidenciaCelkom = 0; // hodiny odpracované na zákazkach => Evidencia prác
             lfEvidencia = zamestnanec.linksFrom("Evidencia prác", "Zamestnanci");
             if (lfEvidencia) {
