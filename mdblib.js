e17455fdcea37528ae4de16c02a094e68132b5f3
mnauzer <76249162+mnauzer@users.noreply.github.com>
2022-01-10 17:40:51 +0100

    calcZam update

--- mdblib.js
+++ mdblib.js
@@ -23,18 +23,13 @@ const remoteMessage = (
     });
 
 // Popis:                   prepočíta dáta zamestnanca
-const calcZam = ((zamestnanec) => {
+const calcZam = ((zamestnanec, library) => {
     message("Prepočítavam...");
     var nick = zamestnanec.field("Nick");
     var hs = zamestnanec.field("Hodinovka"); // nastavená hodinová sadzba (hs)
     var os = zamestnanec.field("Odpracované sezóny"); // odpracované sezóny (os)
     var sz = zamestnanec.field("Výber sezóny na prepočet"); //sezóny na prepočet
     // premenné na spočítanie
-    var odpracovaneCelkom = 0; // odpracované hodiny celkom => Dochádzka
-    var zarobeneCelkom = 0; // zarobená suma, odpracované hodiny x hodinová mzda => Dochádzka
-    var vyplateneCelkom = 0; // vyplatené mzdy => Pokladňa
-    var evidenciaCelkom = 0; // hodiny odpracované na zákazkach => Evidencia prác
-    var hodnotenieCelkom = 0; // počet hviezdičiek => Dochádzka
     var hodnotenieEvidenciaCelkom = 0; //počet hviezdičiek za zákazky => Evidencia prác
     // konvertuj sz na Array sezony
     var nastaveneSezony = [];
@@ -43,51 +38,64 @@ const calcZam = ((zamestnanec) => {
     }
     nastaveneSezony.forEach((sezona) => {
         // Dochádzka
-        lfDochadzka = zamestnanec.linksFrom("Dochádzka", "Zamestnanci");
-        if (lfDochadzka) {
-            for (var d in lfDochadzka) {
-                var dSezona = lfDochadzka[d].field("Sezóna");
-                if (dSezona == sezona) {
-                    odpracovaneCelkom += lfDochadzka[d].field("Odpracované");
-                    // prebehni pole zamestnanci
-                    var vPraci = lfDochadzka[d].field("Zamestnanci");
-                    for (var i in vPraci) {
-                        if (vPraci[i].field("Nick") == nick) {
-                            zarobeneCelkom += vPraci[i].attr("denná mzda");
-                            hodnotenieCelkom += vPraci[i].attr("hodnotenie");
+        if (library.includes("Dochádzka")) {
+            var odpracovaneCelkom = 0; // odpracované hodiny celkom => Dochádzka
+            var zarobeneCelkom = 0; // zarobená suma, odpracované hodiny x hodinová mzda => Dochádzka
+            var hodnotenieCelkom = 0; // počet hviezdičiek => Dochádzka
+            lfDochadzka = zamestnanec.linksFrom("Dochádzka", "Zamestnanci");
+            if (lfDochadzka) {
+                for (var d in lfDochadzka) {
+                    var dSezona = lfDochadzka[d].field("Sezóna");
+                    if (dSezona == sezona) {
+                        odpracovaneCelkom += lfDochadzka[d].field("Odpracované");
+                        // prebehni pole zamestnanci
+                        var vPraci = lfDochadzka[d].field("Zamestnanci");
+                        for (var i in vPraci) {
+                            if (vPraci[i].field("Nick") == nick) {
+                                zarobeneCelkom += vPraci[i].attr("denná mzda");
+                                hodnotenieCelkom += vPraci[i].attr("hodnotenie");
+                            }
                         }
                     }
                 }
             }
+            zamestnanec.set("Odpracované", odpracovaneCelkom);
+            zamestnanec.set("Zarobené", zarobeneCelkom);
+            zamestnanec.set("Dochádzka", hodnotenieCelkom);
+            zamestnanec.set("Preplatok/Nedoplatok", zarobeneCelkom - zamestnanec.field("Vyplatené"));
         }
         // Mzdy
-        lfPlatby = zamestnanec.linksFrom("Pokladňa", "Zamestnanec");
-        if (lfPlatby) {
-            for (var d in lfPlatby) {
-                var dSezona = lfPlatby[d].field("Sezóna");
-                if (dSezona == sezona) {
-                    vyplateneCelkom += lfPlatby[d].field("Výdavok bez DPH");
+        if (library.includes("Platby")) {
+            var vyplateneCelkom = 0; // vyplatené mzdy => Pokladňa
+            lfPlatby = zamestnanec.linksFrom("Pokladňa", "Zamestnanec");
+            if (lfPlatby) {
+                for (var d in lfPlatby) {
+                    var dSezona = lfPlatby[d].field("Sezóna");
+                    if (dSezona == sezona) {
+                        vyplateneCelkom += lfPlatby[d].field("Výdavok bez DPH");
+                    }
                 }
             }
+            zamestnanec.set("Vyplatené", vyplateneCelkom);
+            zamestnanec.set("Preplatok/Nedoplatok", zamestnanec.field("Zarobené") - vyplateneCelkom);
         }
+
         // Evidencia prác
-        lfEvidencia = zamestnanec.linksFrom("Evidencia prác", "Zamestnanci");
-        if (lfEvidencia) {
-            for (var d in lfEvidencia) {
-                var dSezona = lfEvidencia[d].field("Sezóna");
-                if (dSezona == sezona) {
-                    evidenciaCelkom += lfEvidencia[d].field("Odpracované/os");
-                    // hodnotenieEvidenciaCelkom +=
+        if (library.includes("Práce")) {
+            var evidenciaCelkom = 0; // hodiny odpracované na zákazkach => Evidencia prác
+            lfEvidencia = zamestnanec.linksFrom("Evidencia prác", "Zamestnanci");
+            if (lfEvidencia) {
+                for (var d in lfEvidencia) {
+                    var dSezona = lfEvidencia[d].field("Sezóna");
+                    if (dSezona == sezona) {
+                        evidenciaCelkom += lfEvidencia[d].field("Odpracované/os");
+                        // hodnotenieEvidenciaCelkom +=
+                    }
                 }
             }
+            zamestnanec.set("Odpracované na zákazkach", evidenciaCelkom);
         }
     });
-    zamestnanec.set("Odpracované", odpracovaneCelkom);
-    zamestnanec.set("Zarobené", zarobeneCelkom);
-    zamestnanec.set("Vyplatené", vyplateneCelkom);
-    zamestnanec.set("Preplatok/Nedoplatok", zarobeneCelkom - vyplateneCelkom);
-    zamestnanec.set("Odpracované na zákazkach", evidenciaCelkom);
-    zamestnanec.set("Dochádzka", hodnotenieCelkom);
     //zamestnanec.set("Zákazky", hodnotenieEvidenciaCelkom);
 });
 // End of file: 10.01.2022, 15:25
