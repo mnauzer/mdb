9716bd895f7769232b481a5533e753d9487747ff
mnauzer <76249162+mnauzer@users.noreply.github.com>
2022-03-25 22:09:09 +0100

    test

--- krajinkaLib.js
+++ krajinkaLib.js
@@ -3,7 +3,7 @@
 // Dátum:                   06.03.2022
 // Popis:                   knižnica krajinka app
 function verziaKrajinkaLib() {
-    var verzia = "0.1.02";
+    var verzia = "0.1.03";
     //message("cpLibrary v." + verzia);
     return verzia;
 }
