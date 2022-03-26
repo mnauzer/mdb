60b87497752572f4f267ab6b2fd1e7cd69ef9053
mnauzer <76249162+mnauzer@users.noreply.github.com>
2022-03-26 07:09:11 +0100

    UPDATES

--- cpLibrary.js
+++ cpLibrary.js
@@ -6,7 +6,7 @@
 // vytvorenie nových výkazov prác z cenovej ponuky
 // vytvorí výkaz pre každý diel cenovej ponuky
 function verziaKniznice() {
-    var verzia = "0.2.01";
+    var verzia = "0.2.02";
     //message("cpLibrary v." + verzia);
     return verzia;
 }
