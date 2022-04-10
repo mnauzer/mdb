cbf9518addecf4182ca61c21449a6dd84ddddf84
mnauzer <76249162+mnauzer@users.noreply.github.com>
2022-04-10 21:50:20 +0200

    up

--- zakazkyLibrary.js
+++ zakazkyLibrary.js
@@ -651,15 +651,15 @@ const zakazkaToJson = zakazka => {
     var result = "";
     var f = file("/sdcard/" + zakazka.field("Číslo") + "_file.json");
     var odberatel = zakazka.field("Klient")[0];
-    f.writeLine("{'Odberateľ':{");
-    f.writeLine("'Nick'" + ":'" + odberatel.field("Nick") + "',");
-    f.writeLine("'Meno'" + ":'" + odberatel.field("Meno") + "',");
-    f.writeLine("'Priezvisko'" + ":'" + odberatel.field("Priezvisko") + "',");
-    f.writeLine("'Titul'" + ":'" + odberatel.field("Titul") + "',");
-    f.writeLine("'Ulica'" + ":'" + odberatel.field("Ulica") + "',");
-    f.writeLine("'PSČ'" + ":'" + odberatel.field("PSČ") + "',");
-    f.writeLine("'Mesto'" + ":'" + odberatel.field("Mesto") + "'}");
-    f.writeLine("}");
+    f.writeLine('{"Odberateľ":{');
+    f.writeLine('"Nick"' + ':"' + odberatel.field("Nick") + '",');
+    f.writeLine('"Meno"' + ':"' + odberatel.field("Meno") + '",');
+    f.writeLine('"Priezvisko"' + ':"' + odberatel.field("Priezvisko") + '",');
+    f.writeLine('"Titul"' + ':"' + odberatel.field("Titul") + '",');
+    f.writeLine('"Ulica"' + ':"' + odberatel.field("Ulica") + '",');
+    f.writeLine('"PSČ"' + ':"' + odberatel.field("PSČ") + '",');
+    f.writeLine('"Mesto"' + ':"' + odberatel.field("Mesto") + '"}');
+    f.writeLine('}');
     f.close();                      // Close & save. Until closed,
     //   the file is still empty
     //var a = f.readLines();
