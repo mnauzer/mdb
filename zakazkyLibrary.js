69a2d7891af1aefd83a0918e712e1ee08c24405f
mnauzer <76249162+mnauzer@users.noreply.github.com>
2022-04-10 21:25:32 +0200

    up

--- zakazkyLibrary.js
+++ zakazkyLibrary.js
@@ -5,7 +5,7 @@
 function verziaKniznice() {
     var result = "";
     var nazov = "zakazkyLibrary";
-    var verzia = "0.3.57";
+    var verzia = "0.3.58";
     result = nazov + " " + verzia;
     //message("cpLibrary v." + verzia);
     return result;
