4e6dc5eeedb32d7eaa22695e747755dd9e6ebde4
mnauzer <76249162+mnauzer@users.noreply.github.com>
2022-04-15 21:08:43 +0200

    update

--- zakazkyLibrary.js
+++ zakazkyLibrary.js
@@ -5,14 +5,14 @@
 function verziaKniznice() {
     var result = "";
     var nazov = "zakazkyLibrary";
-    var verzia = "0.3.13";
+    var verzia = "0.3.14";
     result = nazov + " " + verzia;
     //message("cpLibrary v." + verzia);
     return result;
 }
 
 const prepocetZakazky = zakazka => {
-    var verzia = "0.3.01";
+    var verzia = "0.3.02";
     var vKniznica = verziaKniznice();
     var vKrajinkaLib = verziaKrajinkaLib();
     message("PREPOČÍTAJ ZÁKAZKU v. " + verzia + "\nv. " + vKniznica + "\nv. " + vKrajinkaLib);
@@ -250,7 +250,6 @@ const generujVyuctovanie = zakazka => {
     var vKniznica = verziaKniznice();
     var vKrajinkaLib = verziaKrajinkaLib();
     message("GENERUJ VYÚČTOVANIE v." + verzia + "\nv." + vKniznica + "\nv." + vKrajinkaLib);
-    var cp = zakazka.field("Cenová ponuka")[0];
     var typ = cp.field("Typ cenovej ponuky");
     var uctovanieDPH = zakazka.field("Účtovanie DPH");
     // inicializácia
@@ -268,6 +267,7 @@ const generujVyuctovanie = zakazka => {
         var praceDPH = 0;
         var praceSDPH = mclChecked(uctovanieDPH, "Práce");
         for (var vp = 0; vp < vykazyPrac.length; vp++) {
+            var typ = vykayPrac[vp].field("Typ vkazu");
             if (typ == "Hodinovka" || vykazyPrac[vp].field("Popis") == "Práce navyše") {
                 praceCelkomBezDPH += prepocitatVykazPraceHzs(vykazyPrac[vp], praceSDPH, sadzbaDPH);
             } else {
