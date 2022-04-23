602975cf9b428fc58cf88ef9756fdaac072a4b9e
mnauzer <76249162+mnauzer@users.noreply.github.com>
2022-04-23 10:33:34 +0200

    update

--- vykazPracLibrary.js
+++ vykazPracLibrary.js
@@ -1,7 +1,7 @@
 function verziaKniznice() {
     var result = "";
     var nazov = "vykazPracLibrary";
-    var verzia = "0.2.03";
+    var verzia = "0.2.05";
     result = nazov + " " + verzia;
     return result;
 }
