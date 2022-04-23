64a99429289240b8d4c774b1d746ccafc229a32b
mnauzer <rasto.skubal+github@gmail.com>
2022-04-23 07:08:44 +0200

    update

--- vykazStrojovLibrary.js
+++ vykazStrojovLibrary.js
@@ -1,12 +1,12 @@
 function verziaKniznice() {
     var result = "";
     var nazov = "vykazStrojovLibrary";
-    var verzia = "0.2.05";
+    var verzia = "0.2.06";
     result = nazov + " " + verzia;
     return result;
 }
 
-const prepocitatVykazStrojov = (zaznam, sDPH = true) => {
+const prepocitatVykazStrojov = (zaznam, uctovatDPH = true) => {
     var stroje = zaznam.field(FIELD_STROJE);
     var sumaBezDPH = 0;
     var dph = 0;
@@ -14,25 +14,25 @@ const prepocitatVykazStrojov = (zaznam, sDPH = true) => {
     var CPsumaBezDPH = 0;
     var CPdph = 0;
     var CPsumaCelkomSDPH = 0;
-    var sezona = zaznam.field(FIELD_SEZONA);
-    if (!sezona || sezona == 0) {
-        sezona = zaznam.field(FIELD_DATUM).getFullYear();
-        zaznam.set(FIELD_SEZONA, sezona);
-    }
 
-    if (stroje) {
+    if (stroje.length > 0) {
         for (var p = 0; p < stroje.length; p++) {
             // výpočet ceny
             var mnozstvoCP = stroje[p].attr("množstvo z cp");
             var dodaneMnozstvo = stroje[p].attr("prevádzka mth");
             var cena = stroje[p].attr("účtovaná sadzba");
-            var cenaCelkom = dodaneMnozstvo ? dodaneMnozstvo * cena : mnozstvoCP * cena;
+            var cenaCelkom = dodaneMnozstvo ? dodaneMnozstvo * cena : null;
             stroje[p].setAttr("cena celkom", cenaCelkom);
             sumaBezDPH += cenaCelkom;
             //výpočet ceny z cp
-            CPsumaCelkomSDPH += (mnozstvoCP * cena);
+            CPsumaBezDPH += (mnozstvoCP * cena);
         }
-        if (sDPH) {
+        if (uctovatDPH) {
+            var sezona = zaznam.field(FIELD_SEZONA);
+            if (!sezona || sezona == 0) {
+                sezona = zaznam.field(FIELD_DATUM).getFullYear();
+                zaznam.set(FIELD_SEZONA, sezona);
+            }
             var sadzbaDPH = libByName(DB_ASSISTENT).find(sezona)[0].field("Základná sadzba DPH") / 100;
             dph = sumaBezDPH * sadzbaDPH;
             sumaCelkom = sumaBezDPH + dph;
@@ -47,4 +47,5 @@ const prepocitatVykazStrojov = (zaznam, sDPH = true) => {
         zaznam.set("CP DPH", CPdph);
         zaznam.set("CP Suma s DPH", CPsumaCelkomSDPH);
     }
+    return [sumaBezDPH, dph, sumaCelkom];
 }
