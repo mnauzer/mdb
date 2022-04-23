ad97e5a8b0f0d9868d4e4b50a49a0cb14bfd55d6
mnauzer <76249162+mnauzer@users.noreply.github.com>
2022-04-23 14:02:01 +0200

    update

---
+++ fieldsLib.js
@@ -0,0 +1,51 @@
+// fields
+// spolocne
+const FIELD_CENOVA_PONUKA = "Cenová ponuka"; // link to entry
+const FIELD_ZAKAZKA = "Zákazka";
+const FIELD_VYUCTOVANIE = "Vyúčtovanie";
+const FIELD_TYP_VYKAZU = "Typ výkazu";
+const FIELD_POPIS = "Popis";
+const FIELD_CISLO = "Číslo";
+// nastavenia
+const FIELD_SEZONA = "sezóna";
+const FIELD_DEBUG = "debug";
+const FIELD_BACKROUND_COLOR = "background color";
+const FIELD_ENTRY_COLOR = "entry color";
+const FIELD_VIEW = "Tlač";
+//
+const FIELD_PRACE = "Práce";
+const FIELD_MATERIAL = "Materiál";
+const FIELD_STROJE = "Stroje";
+const FIELD_DOPRAVA = "Doprava";
+// diely polozky
+const FIELD_TRAVNIK = "Trávnik";
+const FIELD_VYSADBY = "Výsadby";
+const FIELD_RASTLINY = "Rastliny";
+const FIELD_ZAVLAZOVANIE = "Zavlažovanie";
+const FIELD_JAZIERKO = "Jazierko";
+const FIELD_KAMEN = "Kameň";
+const FIELD_NESTANDARDNE = "Neštandardné";
+const FIELD_SUBDODAVKY = "Subdodávky";
+// diely hzs
+const FIELD_ZAHRADNICKE_PRACE = "Záhradnícke práce";
+const FIELD_SERVIS_ZAVLAZOVANIA = "Servis zavlažovania";
+const FIELD_KONZULTACIE = "Konzultácie a poradenstvo"
+const FIELD_PRACE_NAVYSE = "Práce navyše";
+const FIELD_DATUM = "Dátum";
+// zamestnanci
+const FIELD_ZAMESTNANCI = "Zamestnanci";
+const FLD_ZAMESTNANCI = 'field("Zamestnanci")';
+const FIELD_HODINOVKA = "Hodinovka";
+const FLD_HODINOVKA = 'field("Hodinovka")';
+// zakazky
+const FIELD_UCTOVANIE_DPH = "Účtovanie DPH";
+
+// words
+const W_ZAKAZKA = "Zákazka";
+const W_PRACE = "Práce";
+const W_PRACE_NAVYSE = "Práce navyše";
+const W_DOPRAVA = "Doprava";
+const W_MATERIAL = "Materiál";
+const W_STROJE = "Stroje";
+const W_HODINOVKA = "Hodinovka";
+const W_POLOZKY = "Položky";
