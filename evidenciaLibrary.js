4a164a9838d9461d364c2c2aaccdc256dcbd468d
mnauzer <rasto.skubal+github@gmail.com>
2022-04-26 22:50:08 +0200

    update

--- evidenciaLibrary.js
+++ evidenciaLibrary.js
@@ -105,19 +105,20 @@ const prepocetZaznamuEvidencie = evidencia => {
                 // ak nemá vygenerovanýv výkaz, vygeneruj nový
                 // ...
             }
-        }
-        var stroje = vykazStrojov.field("Stroje");
-        for (var i = 0; i < vyuzitieStrojov.length; i++) {
-            if (stroje.length > 0) {
-                for (var j = 0; j < stroje.length; j++) {
-                    if (vyuzitieStrojov[i].field("Cena")[0] == vykazStrojov[j]) {
-                        message("true");
-                        stroje[j].setAttr("prevádzka mth", stroje[j].attr("prevádzka mth") + (vyuzitieStrojov[i].attr("doba prevádzky")) / 36000);
-                        break;
-                    } else {
-                        message("false");
-                        vykazStrojov.link("Stroje", vyuzitieStrojov[i].field("Cena")[0]);
-                        stroje[j].setAttr("prevádzka mth", vyuzitieStrojov[i].attr("doba prevádzky"));
+        } else {
+            var stroje = vykazStrojov.field("Stroje");
+            for (var i = 0; i < vyuzitieStrojov.length; i++) {
+                if (stroje.length > 0) {
+                    for (var j = 0; j < stroje.length; j++) {
+                        if (vyuzitieStrojov[i].field("Cena")[0] == vykazStrojov[j]) {
+                            message("true");
+                            stroje[j].setAttr("prevádzka mth", stroje[j].attr("prevádzka mth") + (vyuzitieStrojov[i].attr("doba prevádzky")) / 36000);
+                            break;
+                        } else {
+                            message("false");
+                            vykazStrojov.link("Stroje", vyuzitieStrojov[i].field("Cena")[0]);
+                            stroje[j].setAttr("prevádzka mth", vyuzitieStrojov[i].attr("doba prevádzky"));
+                        }
                     }
                 }
             }
