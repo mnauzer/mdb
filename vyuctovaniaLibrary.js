f4d283c013606859b0bd3c28709b2a23d02885d1
mnauzer <rasto.skubal+github@gmail.com>
2022-04-26 12:02:24 +0200

    update

--- vyuctovaniaLibrary.js
+++ vyuctovaniaLibrary.js
@@ -562,31 +562,14 @@ const nalinkujPrace = (vyuctovanie, vykazPrac) => {
         var cena = polozkyVykazPrac[m].attr("cena");
         var cenaCelkom = polozkyVykazPrac[m].attr("cena celkom");
         vyuctovanie.link(popis, polozkyVykazPrac[m])
-        vyuctovanie.field(popis)[m].setAttr("množstvo", mnozstvo);
+        vyuctovanie.field(popis)[m].setAttr("dodané množstvo", mnozstvo);
         vyuctovanie.field(popis)[m].setAttr("cena", cena);
         vyuctovanie.field(popis)[m].setAttr("cena celkom", cenaCelkom);
         vykazPracCelkom += cenaCelkom;
         // nastav príznak Tlač
         setTlac(polozkyVykazPrac[m]);
     }
-    // } else {
-    //     // práce navyše
-    //     var praceNavyse = vykazPrac.field("Práce sadzby")[0];
-    //     var hodinCelkom = 0;
-    //     var uctovanaSadzba = vykazPrac.field(FIELD_CENOVA_PONUKA)[0].field(popis)[0].attr("sadzba");
-    //     var cenaCelkom = 0;
-    //     vyuctovanie.link(popis, praceNavyse);
-    //     var evidenciaLinks = vykazPrac.linksFrom(DB_EVIDENCIA_PRAC, "Výkaz prác");
-    //     for (var e = 0; e < evidenciaLinks.length; e++) {
-    //         vyuctovanie.link("Rozpis", evidenciaLinks[e]);
-    //         vyuctovanie.field("Rozpis")[e].setAttr("popis prác", evidenciaLinks[e].attr("popis prác"));
-    //         vyuctovanie.field("Rozpis")[e].setAttr("počet hodín", evidenciaLinks[e].attr("počet hodín"));
-    //         hodinCelkom += evidenciaLinks[e].attr("počet hodín");
-    //     }
-    //     cenaCelkom = hodinCelkom * uctovanaSadzba;
-    //     vyuctovanie.field(popis)[0].setAttr("cena celkom", cenaCelkom);
 
-    // }
     vyuctovanie.set(popis + " celkom", vykazPracCelkom);
     return vykazPracCelkom;
 }
