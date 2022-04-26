20eca54eadd28afb4914210fff311679db4600ef
mnauzer <rasto.skubal+github@gmail.com>
2022-04-26 22:45:15 +0200

    1

--- evidenciaLibrary.js
+++ evidenciaLibrary.js
@@ -6,7 +6,7 @@
 function verziaKniznice() {
     var result = "";
     var nazov = "evidenciaLibrary";
-    var verzia = "0.2.15";
+    var verzia = "0.2.16";
     result = nazov + " " + verzia;
     //message("cpLibrary v." + verzia);
     return result;
