5919ce8bb4efeb7d0d4dcee1ae4167320c524c18
mnauzer <rasto.skubal+github@gmail.com>
2022-04-27 08:19:17 +0200

    UP

--- vykazStrojovLibrary.js
+++ vykazStrojovLibrary.js
@@ -1,7 +1,7 @@
 const verziaVykazStrojov = () => {
     var result = "";
     var nazov = "vykazStrojovLibrary";
-    var verzia = "0.2.13";
+    var verzia = "0.2.14";
     result = nazov + " " + verzia;
     return result;
 }
