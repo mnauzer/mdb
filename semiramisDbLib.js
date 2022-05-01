5c9c21ff389b748c91d62c7d4d219d052fd74bd7
mnauzer <rasto.skubal+github@gmail.com>
2022-05-01 13:01:21 +0200

    update

--- semiramisDbLib.js
+++ semiramisDbLib.js
@@ -4,32 +4,32 @@ const DB_ASSISTENT = "SEMIRAMIS APP";
 const DB_ASSISTENT_DATABAZY = "SEMIRAMIS APP DATABÁZY";
 // projekty
 const DB_CENOVE_PONUKY = "Cenové ponuky";
-const DB_ZAKAZKY = "Zákazky";
-const DB_VYUCTOVANIA = "Vyúčtovania";
-const DB_CENNIK_PRAC = "Cenník prác";
-const DB_SKLAD = "Sklad Krajinka";
+const DB_ZAKAZKY = "Zákazky Semiramis";
+const DB_VYUCTOVANIA = "Vyúčtovania Semiramis";
+const DB_CENNIK_PRAC = "Cenník prác Semiramis";
+const DB_SKLAD = "Sklad Semiramis";
 const DB_PLAN_PRAC = "Plán prác";
 // administratíva
-const DB_VYKAZY_PRAC = "Výkaz prác";
-const DB_VYDAJKY_MATERIALU = "Výdajky";
+const DB_VYKAZY_PRAC = "Výkaz prác Semiramis";
+const DB_VYDAJKY_MATERIALU = "Výdajky Semiramis";
 const DB_VYKAZY_STROJOV = "Výkaz strojov";
 const DB_INVENTURY = "Invenúry";
-const DB_PRIJEMKY = "Príjemky";
+const DB_PRIJEMKY = "Príjemky Semiramis";
 const DB_REZERVACIE = "Rezervácie";
 const DB_OBJEDNAVKY = "Objednávky";
 // evidencia
-const DB_POKLADNA = "Pokladňa";
-const DB_EVIDENCIA_PRAC = "Evidencia prác";
-const DB_DOCHADZKA = "Dochádzka";
+const DB_POKLADNA = "Pokladňa Semiramis";
+const DB_EVIDENCIA_PRAC = "Evidencia prác Semiramis";
+const DB_DOCHADZKA = "Dochádzka Semiramis";
 const DB_KNIHA_JAZD = "Kniha jázd";
 const DB_ZASTAVKY = "Zastávky";
 // databázy
-const DB_ZAMESTNANCI = "Zamestnanci";
-const DB_KLIENTI = "Klienti";
+const DB_ZAMESTNANCI = "Zamestnanci Semiramis";
+const DB_KLIENTI = "Klienti Semiramis";
 const DB_DODAVATELIA = "Dodávatelia";
 const DB_PARTNERI = "Partneri";
 const DB_MIESTA = "Miesta";
-const DB_UCTY = "Účty";
+const DB_UCTY = "Účty Semiramis";
 const DB_STROJE = "Stroje";
 const DB_VOZIDLA = "Vozidlá";
 const DB_RASTLINY = "Databáza rastlín";
