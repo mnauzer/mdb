af323c1d4cd4cb5edf8fec5f7301660e997a9e89
mnauzer <rasto.skubal+github@gmail.com>
2022-05-01 13:06:28 +0200

    update

---
+++ dbLib.js
@@ -0,0 +1,36 @@
+// libraries
+// app
+const DB_ASSISTENT = "KRAJINKA APP";
+const DB_ASSISTENT_DATABAZY = "KRAJINKA APP DATABÁZY";
+// projekty
+const DB_CENOVE_PONUKY = "Cenové ponuky";
+const DB_ZAKAZKY = "Zákazky";
+const DB_VYUCTOVANIA = "Vyúčtovania";
+const DB_CENNIK_PRAC = "Cenník prác";
+const DB_SKLAD = "Sklad";
+const DB_PLAN_PRAC = "Plán prác";
+// administratíva
+const DB_VYKAZY_PRAC = "Výkaz prác";
+const DB_VYDAJKY_MATERIALU = "Výdajky";
+const DB_VYKAZY_STROJOV = "Výkaz strojov";
+const DB_INVENTURY = "Invenúry";
+const DB_PRIJEMKY = "Príjemky";
+const DB_REZERVACIE = "Rezervácie";
+const DB_OBJEDNAVKY = "Objednávky";
+// evidencia
+const DB_POKLADNA = "Pokladňa";
+const DB_EVIDENCIA_PRAC = "Evidencia prác";
+const DB_DOCHADZKA = "Dochádzka";
+const DB_KNIHA_JAZD = "Kniha jázd";
+const DB_ZASTAVKY = "Zastávky";
+// databázy
+const DB_ZAMESTNANCI = "Zamestnanci";
+const DB_KLIENTI = "Klienti";
+const DB_DODAVATELIA = "Dodávatelia";
+const DB_PARTNERI = "Partneri";
+const DB_MIESTA = "Miesta";
+const DB_UCTY = "Účty";
+const DB_STROJE = "Stroje";
+const DB_VOZIDLA = "Vozidlá";
+const DB_RASTLINY = "Databáza rastlín";
+
