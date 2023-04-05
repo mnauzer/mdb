03abc8aa00c35dbd36e5ea047e8f537e90dc5736
mnauzer <rasto.skubal+github@gmail.com>
2023-04-05 16:01:25 +0200

    update

--- dashEvidencia.js
+++ dashEvidencia.js
@@ -5,3 +5,26 @@ function spocitajDochadzku(zamestnanec) {
 function spocitajMzdy(zamestnanec){
 
 }
+var pracujuci = [];
+var fiscalYear = 2003;
+
+const najdiPracujucich = (pracujuci, fiscalYear) => {
+    try{
+        var lib = libByName("Dochádzka");
+        var entries = lib.entries();
+        for (var e = 0;e < entries.length; e++) {
+            var zamestnanci = e.field("Zamestnanci");
+            for (var z = 0;z < zamestnanci.length; z++){
+                if(pracujuci.indexOf(z) === -1) {
+                    pracujuci.push(z);
+                    message('Zamestnanec: ' + z.field('Nick') + ' pridaný');
+                }
+            }
+        }
+
+        return pracujuci;
+
+    } catch (error) {
+        message('Chyba: ' + error);
+    }
+}
