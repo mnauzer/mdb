26d9172bca8786c814dfbce69cd19d92a521034e
mnauzer <rasto.skubal+github@gmail.com>
2023-04-09 09:50:23 +0200

    update colors

--- fakturyPrijate.js
+++ fakturyPrijate.js
@@ -1,6 +1,8 @@
 // scrtipt library pre knižnicu Faktúry Prijaté
 const checkOverdue = (en, date) => {
     if (en.field(DOFA_D_SPL) < date) {
-        en.set(ENT_COLOR, FIREBRICK)
+        en.set(ENT_COLOR, MEM_RED)
+    } else {
+        en.set(ENT_COLOR, MEM_BLUE)
     }
 }
