14fffef491043d2ca8149d00d071dd2cd844b200
mnauzer <rasto.skubal+github@gmail.com>
2023-04-09 21:54:05 +0200

    up

--- libCennikPrac.js
+++ libCennikPrac.js
@@ -6,13 +6,12 @@ function verziaKniznice() {
 }
 
 const setPrice = en => {
-    let links = en.linksFrom("Ceny prác", "Práca");
+    var links = en.linksFrom("Ceny prác", "Práca");
     if (links.lenght > 0) {
-        let date = new Date();
-        let price = lastValid(links, date, "Cena", "Platnosť od");
+        var date = new Date();
+        var price = lastValid(links, date, "Cena", "Platnosť od");
         en.set("Cena bez DPH", price);
     } else {
         message("Položka nemá žiadne zaevidované ceny");
-
     }
 }
