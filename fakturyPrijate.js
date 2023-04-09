d90890165d2d3552ce300a415ec11b10f82c691c
mnauzer <rasto.skubal+github@gmail.com>
2023-04-09 09:56:05 +0200

    up

--- fakturyPrijate.js
+++ fakturyPrijate.js
@@ -1,6 +1,6 @@
 // scrtipt library pre knižnicu Faktúry Prijaté
 const checkOverdue = (en, date) => {
-    if (en.field(!DOFA_PAYED)){
+    if (!en.field(DOFA_PAYED)){
         if (en.field(DOFA_D_SPL) <= date ) {
             en.set(ENT_COLOR, MEM_RED)
         } else {
