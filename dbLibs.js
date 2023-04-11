40f2aaf59ccc13f6c5100070925b0ca83e995fc8
mnauzer <rasto.skubal+github@gmail.com>
2023-04-11 20:47:09 +0200

    up

--- dbLibs.js
+++ dbLibs.js
@@ -40,8 +40,8 @@ const DB_PLANTS = "Databáza rastlín";
 const DB_RCPTS = "Príjemky";
 
 // aGroup
-const DBA_OBL = "aZáväzky";
+//const DBA_OBL = "aZáväzky";
 const DBA_SAL= "aMzdy";
-const DBA_POHLADAVKY = "aPohľadávky";
-const DBA_DOCHADZKA = "aDochádzka";
+//const DBA_POHLADAVKY = "aPohľadávky";
+//const DBA_DOCHADZKA = "aDochádzka";
 //const DBA_WORK = "aPráce";
