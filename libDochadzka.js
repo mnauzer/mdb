c396d805cf9239d7de69cbf9586e7653c7f70829
rasto.bakchus <rasto.skubal@gmail.com>
2023-11-09 22:42:30 +0100

    bug fixes

--- libDochadzka.js
+++ libDochadzka.js
@@ -9,12 +9,9 @@ const updateEntry = en => {
 
 const lastSadzba = (employee, date) => {
     let scriptName = "lastSadzba 23.0.01"
-    let variables = "Záznam: " + employee.name + "\n";
+    let variables = "Zamestnanec: " + employee.name + "\nDátum: " + date
+    let parameters = "employee: " + employee + "\ndate: " + date
     try {
-        var sezona = getSeason(entry());
-        if (checkDebug(sezona)){
-            message("DBGMSG: " + scriptName);
-        } 
         // odfiltruje záznamy sadzby z vyšším dátumom ako zadaný dátum
         var links = employee.linksFrom("Zamestnanci Sadzby", "Zamestnanec");
         msgGen(DB_DOCHADZKA, "libDochadzka", scriptName, 'Links: " + links.length', variables);
@@ -23,7 +20,6 @@ const lastSadzba = (employee, date) => {
         if (filtered.length < 0) {
             msgGen(DB_DOCHADZKA, "libDochadzka", scriptName, 'Zamestnanec nemá zaevidovanú sadzbu k tomuto dátumu', variables);
         } else {
-    
             filtered.reverse();
         }
         //vyberie a vráti sadzbu z prvého záznamu
@@ -31,18 +27,15 @@ const lastSadzba = (employee, date) => {
         return sadzba;
         
     } catch (error) {
-        errorGen(DB_DOCHADZKA, "libDochadzka.js", scriptName, error, variables);
+        errorGen(DB_DOCHADZKA, "libDochadzka.js", scriptName, error, variables, parameters);
     }
 }
 
 const prepocitatZaznamDochadzky = en => {
     let scriptName = "prepocitatZaznamDochadzky 23.0.01"
+    let variables = "Záznam: " + en.name 
+    let parameters = "en: " + en
     try {
-        var sezona = getSeason(en);
-        if (checkDebug(sezona)){
-            message("DBGMSG: " + scriptName);
-        } 
-        message("Prepočítavám záznam...\nKnižnica: " + verziaKniznice());
         // výpočet pracovnej doby
         var datum = en.field(DATE);
         var prichod = roundTimeQ(en.field("Príchod")); //zaokrúhlenie času na 15min
@@ -112,65 +105,70 @@ const prepocitatZaznamDochadzky = en => {
         en.set("Prestoje", prestojeCelkom);
         message("Hotovo...");
     } catch (error) {
-        var variables = ""
-        errorGen(DB_DOCHADZKA, "libDochadzka.js", scriptName, error, variables);
+        errorGen(DB_DOCHADZKA, "libDochadzka.js", scriptName, error, variables, parameters);
     }
 }
 
 const aSalary = (en, NEW_ENTRY) => {
-    message("Evidujem mzdy v.9");
-    var salaries = libByName(DBA_SAL);
-    var employees = en.field(ATTDC_EMPLOYEES);
-    if (NEW_ENTRY) {
+    let scriptName = "aSalary 23.0.01"
+    let variables = "Záznam: " + en.name + "\nNEW_ENTRY: " + NEW_ENTRY
+    let parameters = "en: " + en + "\nNEW_ENTRY: " + NEW_ENTRY
+    try {
+        var salaries = libByName(DBA_SAL);
+        var employees = en.field(ATTDC_EMPLOYEES);
+        if (NEW_ENTRY) {
 
-    } else {
-        var links = en.linksFrom(DBA_SAL, ATTENDANCE)
-        // skontrolovať či je už záznam nalinkovaný
-        if (links.length > 0){
-            //vymaž nalinkované záznamy
-            message("Mažem už nalinkované záznamy");
-            for (var l = 0; l < links.length; l++){
-                links[l].trash();
-            }
-        }
-    }
-    for (var z = 0; z < employees.length; z++) {
-        var newEntry = new Object();
-        newEntry[DATE] = en.field(DATE);
-        newEntry[NICK] =  employees[z].field(NICK);
-        newEntry["Odpracované"] = en.field("Pracovná doba");
-        newEntry["Sadzba"] =  employees[z].attr("hodinovka");
-        newEntry["Mzda"] =  employees[z].attr("denná mzda");
-        newEntry["Vyplatiť"] =  employees[z].attr("denná mzda");
-        newEntry[SEASON] = en.field(DATE).getFullYear();
-        newEntry[ATTENDANCE] = en;
-        newEntry["Zamestnanec"] = employees[z];
-        salaries.create(newEntry);
-        var entrySalaries = en.linksFrom(DBA_SAL,ATTENDANCE)[0];
-        entrySalaries.field(ATTENDANCE)[0].setAttr("odpracované", en.field("Pracovná doba"));
-        entrySalaries.field("Zamestnanec")[0].setAttr("sadzba", employees[z].attr("hodinovka"));
-        // zauctuj preplatok ak je
-        var preplatokLinks = employees[z].linksFrom("Pokladňa", "Zamestnanec").filter(e => e.field("Preplatok na mzde") == true);
-        if (preplatokLinks.length > 0) {
-            message("Účtujem preplatky");
-            for (var l = 0; l < preplatokLinks.length; l++) {
-                var preplatok = preplatokLinks[l].field("Preplatok");
-                var vyplata = entrySalaries.field("Vyplatiť");
-                if (preplatok >= vyplata) {
-                    entrySalaries.set("Vyplatiť", vyplata);
-                    entrySalaries.link("Platby", preplatokLinks[l]);
-                    entrySalaries.field("Platby")[0].setAttr("suma", vyplata);
-                    preplatok -= vyplata;
-                    preplatokLinks[l].set("Preplatok", preplatok);
-                } else if ( preplatok != 0 && preplatok < vyplata){
-                    entrySalaries.set("Vyplatená mzda", preplatok);
-                    entrySalaries.set("Vyplatiť", vyplata - preplatok);
-                    entrySalaries.link("Platby", en);
-                    entrySalaries.field("Platby")[0].setAttr("suma", preplatok);
-
+        } else {
+            var links = en.linksFrom(DBA_SAL, ATTENDANCE)
+            // skontrolovať či je už záznam nalinkovaný
+            if (links.length > 0){
+                //vymaž nalinkované záznamy
+                message("Mažem už nalinkované záznamy");
+                for (var l = 0; l < links.length; l++){
+                    links[l].trash();
                 }
             }
-
         }
+        for (var z = 0; z < employees.length; z++) {
+            var newEntry = new Object();
+            newEntry[DATE] = en.field(DATE);
+            newEntry[NICK] =  employees[z].field(NICK);
+            newEntry["Odpracované"] = en.field("Pracovná doba");
+            newEntry["Sadzba"] =  employees[z].attr("hodinovka");
+            newEntry["Mzda"] =  employees[z].attr("denná mzda");
+            newEntry["Vyplatiť"] =  employees[z].attr("denná mzda");
+            newEntry[SEASON] = en.field(DATE).getFullYear();
+            newEntry[ATTENDANCE] = en;
+            newEntry["Zamestnanec"] = employees[z];
+            salaries.create(newEntry);
+            var entrySalaries = en.linksFrom(DBA_SAL,ATTENDANCE)[0];
+            entrySalaries.field(ATTENDANCE)[0].setAttr("odpracované", en.field("Pracovná doba"));
+            entrySalaries.field("Zamestnanec")[0].setAttr("sadzba", employees[z].attr("hodinovka"));
+            // zauctuj preplatok ak je
+            var preplatokLinks = employees[z].linksFrom("Pokladňa", "Zamestnanec").filter(e => e.field("Preplatok na mzde") == true);
+            if (preplatokLinks.length > 0) {
+                message("Účtujem preplatky");
+                for (var l = 0; l < preplatokLinks.length; l++) {
+                    var preplatok = preplatokLinks[l].field("Preplatok");
+                    var vyplata = entrySalaries.field("Vyplatiť");
+                    if (preplatok >= vyplata) {
+                        entrySalaries.set("Vyplatiť", vyplata);
+                        entrySalaries.link("Platby", preplatokLinks[l]);
+                        entrySalaries.field("Platby")[0].setAttr("suma", vyplata);
+                        preplatok -= vyplata;
+                        preplatokLinks[l].set("Preplatok", preplatok);
+                    } else if ( preplatok != 0 && preplatok < vyplata){
+                        entrySalaries.set("Vyplatená mzda", preplatok);
+                        entrySalaries.set("Vyplatiť", vyplata - preplatok);
+                        entrySalaries.link("Platby", en);
+                        entrySalaries.field("Platby")[0].setAttr("suma", preplatok);
+
+                    }
+                }
+
+            }
+        }
+    } catch (error) {
+        errorGen(DB_DOCHADZKA, "libDochadzka.js", scriptName, error, variables, parameters);
     }
 }
