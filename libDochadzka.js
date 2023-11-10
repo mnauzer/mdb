9fb4a7073884aa6e4fbae980beec45e76b77fc5e
mnauzer <rasto.skubal+github@gmail.com>
2023-11-10 08:25:21 +0100

    bug fixes

--- libDochadzka.js
+++ libDochadzka.js
@@ -1,7 +1,7 @@
 
 const newEntryDochadzka = en => {
     let scriptName = "updateEntryDochadzka 23.0.04"
-    let mementoDatabase = lib().title()
+    let mementoDatabase = lib().title
     let variables = "Záznam: " + en.name + "mementoDatabase: " + mementoDatabase
     let parameters = "en: " + en + "mementoDatabase: " + mementoDatabase
     message("New Entry")
