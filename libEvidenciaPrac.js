08f54f3affb3ae72be833ff0e826d9b4c89f888a
rasto.bakchus <rasto.skubal@gmail.com>
2023-11-12 15:51:37 +0100

    update

--- libEvidenciaPrac.js
+++ libEvidenciaPrac.js
@@ -74,7 +74,7 @@ const evidenciaSadzbaPrace = (vykazPrac, hodinyCelkom) => {
 };
 
 const btnFill = () => {
-    let scriptName ="btnFill 23.0.27"
+    let scriptName ="btnFill 23.0.28"
     let variables = "Záznam: " + entry().name 
     let parameters = "en: " + entry()
     let txtMsg = ""
@@ -105,6 +105,7 @@ const btnFill = () => {
             message(links.length)
             if (element != undefined)
             message(element)
+            entry().link(element, links[0])
         });
     } catch (error) {
         errorGen(DB_EVIDENCIA_PRAC, "libEvidenciaPrac.js", scriptName, error, variables, parameters);
