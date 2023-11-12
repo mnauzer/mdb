75d183567bc171e958b74f42f3ddcb55311da5ae
rasto.bakchus <rasto.skubal@gmail.com>
2023-11-12 14:34:02 +0100

    up

--- libEvidenciaPrac.js
+++ libEvidenciaPrac.js
@@ -74,7 +74,7 @@ const evidenciaSadzbaPrace = (vykazPrac, hodinyCelkom) => {
 };
 
 const btnFill = () => {
-    let scriptName ="btnFill 23.0.16"
+    let scriptName ="btnFill 23.0.17"
     let variables = "Záznam: " + entry().name 
     let parameters = "en: " + entry()
     let txtMsg = ""
@@ -97,23 +97,23 @@ const btnFill = () => {
         switch (evidovat) {
             case "Výkaz prác":
                 message("Výkaz prác")
-                entry().link("Výkaz prác", entry().field(FIELD_ZAKAZKA)[0].linksFrom("Výkaz prác", "Zákazka")[0])
+                entry().link("Výkaz prác", entry().field(FIELD_ZAKAZKA)[0].linksFrom("Výkaz prác", "Zákazka"))
                 break
             case "Stavebný denník":
                     message("Stavebný denník")
-                    entry().link("Stavebný denník", entry().field(FIELD_ZAKAZKA)[0].linksFrom("Stavebný denník", "Zákazka")[0])
+                    entry().link("Stavebný denník", entry().field(FIELD_ZAKAZKA)[0].linksFrom("Stavebný denník", "Zákazka"))
                 break
             case "Materiál":
-                entry().link("Výkaz materiálu", entry().field(FIELD_ZAKAZKA)[0].linksFrom("Výkaz materiálu", "Zákazka")[0])
+                entry().link("Výkaz materiálu", entry().field(FIELD_ZAKAZKA)[0].linksFrom("Výkaz materiálu", "Zákazka"))
                 break
             case "Položky":
                 //entry().link("Položky", entry().field(FIELD_ZAKAZKA)[0].linksFrom("Položky", "Zákazka")[0])
                 break
             case "Dopravu":
-                entry().link("Výkaz dopravy", entry().field(FIELD_ZAKAZKA)[0].linksFrom("Výkaz dopravy", "Zákazka")[0])
+                entry().link("Výkaz dopravy", entry().field(FIELD_ZAKAZKA)[0].linksFrom("Výkaz dopravy", "Zákazka"))
                 break
             case "Stroje":
-                entry().link("Výkaz strojov", entry().field(FIELD_ZAKAZKA)[0].linksFrom("Výkaz strojov", "Zákazka")[0])
+                entry().link("Výkaz strojov", entry().field(FIELD_ZAKAZKA)[0].linksFrom("Výkaz strojov", "Zákazka"))
                 break
             case "Subdodávky":
                 break
