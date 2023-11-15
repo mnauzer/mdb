d7db548b35e045fd7d656658d0de43d3b8c3f7f8
mnauzer <rasto.skubal+github@gmail.com>
2023-11-15 19:47:57 +0100

    update

--- libAdministrativa.js
+++ libAdministrativa.js
@@ -104,7 +104,7 @@ const updateObligations = en => {
 
 // ZÁVAZKY
 const newEntryZavazky = (employee, en, sum) => {
-    let scriptName = "newEntryZavazky 23.0.06"
+    let scriptName = "newEntryZavazky 23.0.07"
     let parameters = "employee: " + employee + "\nen: " + en + "\nsum: " + sum
     let memLib = LIB_ZVK
     let variables = "user: " + user() + "\nmemLib: " + memLib
@@ -140,8 +140,8 @@ const newEntryZavazky = (employee, en, sum) => {
         let novyZavazok = zavazky.entries()[0]
         if (novyZavazok.field(NUMBER_ENTRY) == newNumber[1]) {
             logTxt += "\nNový záznam záväzku č. " + newNumber[0]
-            appDB.setAttr("posledné číslo", newNumber[0])
-            appDB.setAttr("následujúce číslo", newNumber[0] + 1)
+            appDB.setAttr("posledné číslo", newNumber[1])
+            appDB.setAttr("následujúce číslo", newNumber[1] + 1)
             novyZavazok.set(VIEW, VIEW_PRINT)
         } else {
             logTxt += "\nNový záznam nebol vytvorený"
