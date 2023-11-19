0f09c9a42f8cfefd7de032e2e3c17d50279c37b4
mnauzer <rasto.skubal+github@gmail.com>
2023-11-19 10:02:03 +0100

    update

--- appScripts.js
+++ appScripts.js
@@ -3,7 +3,7 @@ const filterTest = () => {
     let testArray = [1,2,3,15,25,11,7,18,125,3,4]
     message(testArray)
     const filtered = testArray.filter(entry => {return entry < 20})
-    message(filtered)
+    message(filtered.sort())
 }
 
 
