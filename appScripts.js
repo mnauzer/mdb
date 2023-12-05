b159b2d8e7be0ed383f2a497a36903056ced78db
mnauzer <rasto.skubal+github@gmail.com>
2023-12-05 07:55:50 +0100

    refaktoring

--- appScripts.js
+++ appScripts.js
@@ -3,31 +3,32 @@ const app = {
     // app store
     name: 'ASISTANTO',
     version: '23.1.0001',
+    app: "ASISTANTO",
+    db: "ASISTANTO DB",
+    errors: "ASISTANTO Errors",
+    tenants: "ASISTANTO Tenants",
+    scripts: "ASISTANTO Scripts",
+    todo: "ASISTANTO ToDo",
     season: null,
     openLib: {
         name: null,
         db: null,
     },
-    lib: {
-        app: "ASISTANTO",
-        db: "ASISTANTO DB",
-        errors: "ASISTANTO Errors",
-        tenants: "ASISTANTO Tenants",
-        scripts: "ASISTANTO Scripts",
-        todo: "ASISTANTO ToDo",
-    },
+    en: null,
+    enD: null,
+    lib: null,
 }
 
 const get = {
     // app getters
     season(){
-        app.season = libByName(app.lib.tenants).find("KRAJINKA")[0].field("default season")
+        app.season = libByName(app.tenants).find("KRAJINKA")[0].field("default season")
     },
     name(){
         app.openLib.name = lib().name
     },
     db(){
-        const dbLib = libByName(app.lib.app).find(app.season)[0].field("Databázy")
+        const dbLib = libByName(app.app).find(app.season)[0].field("Databázy")
         app.openLib.name = lib().name;
         app.openLib.db = dbLib.filter(en => en.field("Názov") == app.openLib.name)
     },
@@ -65,11 +66,11 @@ const logAppVariableStore = () => {
         +'\napp.version: ' + app.version
         +'\napp.season: ' +  app.season
         +'\napp.openLib.name: ' +  app.openLib.name
-        const errorLib = libByName(app.lib.errors)
+        const errorLib = libByName(app.errors)
         const newError = new Object()
         newError['type'] = 'log'
         newError['date'] = new Date()
-        newError['memento library'] = app.name
+        newError['memento library'] = app.openLib.name
         newError['script'] = 'logAppVariableStore'
         newError['text'] = 'app store variables'
         newError['variables'] = storeVariables
