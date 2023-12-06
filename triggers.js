10772a92cfe87e64bdb1c35ace63c5ddc6611f2f
rasto.bakchus <rasto.skubal@gmail.com>
2023-12-06 10:36:32 +0100

    update

--- triggers.js
+++ triggers.js
@@ -12,12 +12,11 @@ const libOpen = () => {
         createErrorEntry(app.runningScript, error)
     }
 }
-const newEntry = (initScript)  => {
+const newEntry = (en, initScript)  => {
     app.runningScript = 'newEntry()'
     app.libFile = 'triggers.js'
     app.initScript = initScript
     try {
-        let en = app.openLib.end
         message("Nový záznam >> " + app.openLib.name)
         en.set(VIEW, VIEW_EDIT)
         en.set(DATE, new Date())
