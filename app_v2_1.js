12fecae64a9d283529c3c4a438417726fe4993b4
rasto.bakchus <rasto.skubal@gmail.com>
2025-04-18 19:03:05 +0200

    `Update version number in CONFIG data`

--- app_v2_1.js
+++ app_v2_1.js
@@ -4,7 +4,7 @@
 const CONFIG = {
     data: {
         name: 'ASISTANTO 2',
-        version: '2.04.0090',
+        version: '2.04.0091',
         app: 'ASISTANTO',
         db: 'ASISTANTO DB',
         errors: 'ASISTANTO Errors',
